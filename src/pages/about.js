9816786a4220fb1ef05cfa4240e5479375dc68cb
Shelley Bassett <m.bassett@y7mail.com>
2021-08-09 09:42:02 +1000

    Subscription Design

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Headers from '../components/Headers'
 
 export default function about() {
   return (
     <div>
       <Layout>
+        <Headers subtitle='Learn more' title='About Brolga Design' />
+        <p>Hire a digital manager for a fraction of the price. <b>Just shoot us an email and we'll get to work for you.</b> </p>
+        <p className='last'>Each of our projects is custom tailored to our clients needs. We always build bespoke digital solutions to help branding stand out from the online noise.</p>
+
+        <Headers subtitle='Why Brolga?' title='Behind the Name' />
+        <p className='last'>A Brolga is an elegant water bird, native to most costal parts of Australia. We chose it as our namesake because of it's uniquly Australian, beautiful to look at and designed for its surroundings; just like our websites. </p>
+
+
+        {/* <Headers subtitle="Who's working for you?" title='Meet Our Team' />
+        <h3>Shelley Bassett</h3>
+        <h4>Owner, Lead Designer</h4> */}
 
       </Layout>
     </div>
--- src/pages/packages/index.js
+++ src/pages/packages/index.js
@@ -4,43 +4,59 @@ import Layout from '../../components/Layout'
 import Headers from '../../components/Headers'
 import Cards from '../../components/Cards'
 import Button from '../../components/Button'
+import * as Icon from 'react-feather'
 
 export default function index() {
   return (
     <div>
         <Layout>
-            <Headers subtitle='Get started with' title='The Works Design Package'/>
+            
+            <Headers subtitle='Get started with' title='The Complete Design Package'/>
             <div className='cardwrapper'>
-                <Cards 
-                    title='Branding Refresher'
-                    desc='Overhaul your branding and get a fresh new face for your business'
-                />
+                <div className='card'>
+                    <p className='last'>Just getting started, needing a complete overhaul or just wanting to start afresh? We're here for whatever your business needs.</p>
+                    
+                    <h3>How The Complete Design Package Works</h3>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    
+                    <Button text='Learn More' class='tinyButton' to=''/>
+                </div>
             </div>
+
             <Headers subtitle='Take it to the next level with' title='Business Booster Packages'/>
             <div className='cardwrapper'>
                 <Cards 
-                    title='Social Media Marketing and Advertising'
+                    title='Digital Marketing and Advertising'
+                    icon='clipboard'
                     desc='Develop a marketing plan that attracts your customers and feels right for your brand'
                     info='/packages/marketing'
                 />
                 <Cards
                     title='Branding Refresher'
+                    icon='brand'
                     desc='Overhaul your branding and get a fresh new face for your business'
                     info='/packages/branding'
                 />
                 <Cards
-                    message='Covid Sale!'
+                    message='Save 20%!'
                     title='Website Overhaul'
+                    icon='layout'
                     desc="Need an online shop? Membership area? We'll help you upgrade your existing site with new features"
                     info='/packages/webdev'
                 />
                 <Cards
                     title='SEO and Performance Optimisation'
+                    icon='search'
                     desc='Update your website with the latest white-hat SEO to boost your search engine rankings'
                     info='/packages/seo'
                 />
             </div>
             <Headers subtitle='Make it your own with' title='DIY Templates and Instructionals'/>
+            <p>Want to DIY a marketing refresh? We've got some simple tools to help. </p>
             <Link to='/resources'><Button text='Shop Now' /></Link>
         </Layout>
     </div>
