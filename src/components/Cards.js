cfa43172efd84964652468d4a46dbd84c282f4b5
Shelley Bassett <m.bassett@y7mail.com>
2021-08-09 09:41:49 +1000

    Subscription design

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import Button from './Button'
+import * as Icon from 'react-feather'
 
 
 
 export default function Cards(props) {
-    
-    const listItems = props.details.map((detail, index) => <li key={index}>{detail}</li>);
+    const iconSvg = {'search': <Icon.Search />, 'layout': <Icon.Layout />, 'clipboard': <Icon.Clipboard />, 'brand': <Icon.Loader />};
+    // const listItems = props.details.map((detail, index) => <li key={index}>{detail}</li>);
 
     return (
         <div className={props.message ? 'card sale' : 'card'} data-content={props.message}>
             <div>
+                {iconSvg[props.icon]}
                 <h3>{props.title}</h3>
                 <hr></hr>
             </div>
-            <div className='details'>
-                <p>{props.desc}</p>
-                <h3>Includes:</h3>
-                <ul>{listItems}</ul>
-            </div>
-            <p>Prices from ${props.price}, get started with a free 30 minute consult</p>
+            <p>{props.desc}</p>
+            {/* <p>Starting from ${props.price}</p> */}
             
-            <Button text='Book Now' to={props.book}/>
             <Button text='Learn More' class='tinyButton' to={props.info}/>
-            
         </div>
     )
 }
@@ -31,7 +27,7 @@ Cards.defaultProps = {
     title: 'Package Name',
     desc: 'Duis suscipit purus vitae lectus pharetra egestas. Mauris auctor pulvinar nibh, sed pellentesque dolor imperdiet quis. Aenean finibus commodo orci, ac blandit mauris ultrices quis',
     price: '200',
-    details: ['weekly backups', 'domain and hosting', 'basic email support'],
+    icon: '',
     book: '',
     info: '/packages',
 }
