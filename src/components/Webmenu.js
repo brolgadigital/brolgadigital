ffdcb7420a519a66a0be321f64bce428d4fb472e
Shelley Bassett <m.bassett@y7mail.com>
2021-09-03 09:23:12 +1000

    Added dribble to menu footer

--- src/components/Webmenu.js
+++ src/components/Webmenu.js
@@ -23,6 +23,7 @@ export default function Webmenu() {
                 <ul>
                     <li><OutboundLink href="https://instagram.com/brolgadigital" title="Instagram" aria-label="Instagram"><Icon.Instagram /></OutboundLink></li>
                     <li><OutboundLink href="https://www.facebook.com/brolgadigital" title="Facebook" aria-label="Facebook"><Icon.Facebook /></OutboundLink></li>
+                    <li><OutboundLink href="https://dribbble.com/brolgadigital" title="Dribbble" aria-label="Dribbble"><Icon.Dribbble /></OutboundLink></li>
                     <li><Link to='/faqs' title="FAQ's" aria-label="FAQ's"><Icon.HelpCircle /></Link></li>
                 </ul>
             </div>
