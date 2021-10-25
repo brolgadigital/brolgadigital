2f03188c7e2e80820b9853235e096a368a7a4da5
Shelley Bassett <m.bassett@y7mail.com>
2021-10-26 10:58:12 +1100

    rolledback and workaround for webpack issue 14532

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const config = require('./brolga-config')
 const here = (...p) => path.join(__dirname, ...p)
 
 require("dotenv").config({
-    path: '.env.${process.env.NODE_ENV}',
+  path: '.env.${process.env.NODE_ENV}',
 })
 
 module.exports = {
@@ -36,73 +36,32 @@ module.exports = {
     },
   },
   plugins: [
-    {
-        // keep as first gatsby-source-filesystem plugin for gatsby image support
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            path: `${__dirname}/static/images/uploads`,
-            name: 'uploads',
-        },
-    },
-    {
-        resolve: 'gatsby-transformer-remark',
-        options: {
-          plugins: [
-            {
-                resolve: 'gatsby-remark-relative-images',
-                options: {
-                    name: 'uploads',
-                },
-            },
-            {
-                resolve: 'gatsby-remark-images',
-                options: {
-                    // It's important to specify the maxWidth (in pixels) of
-                    // the content container as this plugin uses this as the
-                    // base for generating different widths of each image.
-                    maxWidth: 2048,
-                },
-            },
-            {
-                resolve: 'gatsby-remark-copy-linked-files',
-                options: {
-                    destinationDir: 'static',
-                },
-            },
-          ],
-        },
-    },
-    {
-        resolve: 'gatsby-plugin-netlify-cms',
-        options: {
-            modulePath: `${__dirname}/src/cms/cms.js`,
-        },
-    },
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     {
-        resolve: "gatsby-plugin-google-analytics",
-        options: {
-            trackingId: "G-VFZCRFC58Q",
-        },
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-VFZCRFC58Q",
+      },
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
-        resolve: "gatsby-plugin-manifest",
-        options: {
-            icon: "src/images/brolga-red-01.png",
-        },
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/brolga-red-01.png",
+      },
     },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-        resolve: "gatsby-source-filesystem",
-        options: {
-            name: "images",
-            path: "./src/images/",
-        },
-        __key: "images",
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
     "react-feather",
     "react-slideshow-image",
