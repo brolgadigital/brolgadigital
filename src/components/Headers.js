5384d6facbcd0d5177aa88d084dd4ce8dc9be1d7
Shelley Bassett <m.bassett@y7mail.com>
2021-11-03 15:30:04 +1100

    added id field

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -4,7 +4,7 @@ export default function Headers(props) {
     return (
         <div className="heading">
             {props.subtitle ? <p>{props.subtitle}</p> : <></> }
-            <h1>{props.title}</h1>
+            {props.id ? <h1 id={props.id}>{props.title}</h1> : <h1>{props.title}</h1> }
         </div>
     )
 }
