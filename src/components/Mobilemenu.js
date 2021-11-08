19c7cb7313e423e18c5b66326a0e21bb37069c57
Shelley Bassett <m.bassett@y7mail.com>
2021-11-08 15:08:24 +1100

    removed resources link from menu

--- src/components/Mobilemenu.js
+++ src/components/Mobilemenu.js
@@ -53,7 +53,6 @@ class Mobilemenu extends React.Component{
                 <li><Link to='/subscriptions' activeClassName="currentPage">Subscriptions</Link></li>
                 <li><Link to='/about' activeClassName="currentPage">About Us</Link></li>
                 <li><Link to='/portfolio' activeClassName="currentPage">Portfolio</Link></li>
-                <li><Link to='/resources' activeClassName="currentPage">Resources</Link></li>
                 <li><Link to='/contact' activeClassName="currentPage">Contact</Link></li>
             </ul>
 
--- src/components/Webmenu.js
+++ src/components/Webmenu.js
@@ -14,7 +14,6 @@ export default function Webmenu() {
                 <li><Link to='/subscriptions' activeClassName="currentPage">Subscriptions</Link></li>
                 <li><Link to='/about' activeClassName="currentPage">About Us</Link></li>
                 <li><Link to='/portfolio' activeClassName="currentPage">Portfolio</Link></li>
-                <li><Link to='/resources' activeClassName="currentPage">Resources</Link></li>
                 <li><Link to='/contact' activeClassName="currentPage">Contact</Link></li>
             </ul>
 
