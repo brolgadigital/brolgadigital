bd90ece8285fae01af820d038b687ad35e47a60c
Shelley Bassett <m.bassett@y7mail.com>
2021-11-15 10:09:48 +1100

    fixed spelling and grammar

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ export default function about() {
         <p className='last'>Now, Brolga Digital is developing into a fully fledged agency, ready to take on any digital challenge that might come up. We aim to create products that enable our clients to reach their full potential in the digital space, be it creative, corporate, commerce or just fun! </p>
 
         <Headers title='Behind the Name' />
-        <p className='last'>A Brolga is an elegant water bird, native to most costal parts of Australia. We chose it as our namesake because of it's uniquly Australian, beautiful to look at and designed for its surroundings; just like our websites. </p>
+        <p className='last'>A Brolga is an elegant water bird, native to most coastal parts of Australia. We chose it as our namesake because of it's uniquely Australian, beautiful to look at and designed for its surroundings; just like our websites. </p>
 
 
         {/* <Headers subtitle="Who's working for you?" title='Meet Our Team' />
