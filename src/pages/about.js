6ce8d61aed12677321675e3eb2c495b5f0660f07
Shelley Bassett <m.bassett@y7mail.com>
2021-11-16 14:46:35 +1100

    fixed some grammar

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,10 +9,10 @@ export default function about() {
       <Layout>
         <Headers title='About Brolga Design' />
         <p>Brolga Digital was founded by Shelley Bassett, who has been managing websites and social media accounts for family, friends and local businesses since 2017. Striking out on her own, she began working with freelance designers, developers and marketers to create the best solutions for her clients.</p>
-        <p className='last'>Now, Brolga Digital is developing into a fully fledged agency, ready to take on any digital challenge that might come up. We aim to create products that enable our clients to reach their full potential in the digital space, be it creative, corporate, commerce or just fun! </p>
+        <p className='last'>Now, Brolga Digital is a fully fledged agency, ready to take on any digital challenge that might come up. We create products that enable our clients to reach their full potential in the digital space, be it creative, corporate, commercial or just fun! </p>
 
         <Headers title='Behind the Name' />
-        <p className='last'>A Brolga is an elegant water bird, native to most coastal parts of Australia. We chose it as our namesake because of it's uniquely Australian, beautiful to look at and designed for its surroundings; just like our websites. </p>
+        <p className='last'>A Brolga is an elegant water bird, native to most coastal parts of Australia. We chose it as our namesake because it's uniquely Australian, beautiful to look at and designed for its surroundings; just like our websites. </p>
 
 
         {/* <Headers subtitle="Who's working for you?" title='Meet Our Team' />
