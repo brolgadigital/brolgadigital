493064c25c58b9b80109abd1b644374b903f53d2
Shelley Bassett <m.bassett@y7mail.com>
2022-02-11 13:50:29 +1100

    Email mailing list

--- src/components/SenderEmail.js
+++ src/components/SenderEmail.js
@@ -1,47 +1,78 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 const SenderEmail = () => {
+    const [fName, setFName] = useState('');
+    const [lName, setLName] = useState('');
+    const [email, setEmail] = useState('');
 
-    const url = new URL(
-        "https://api.sender.net/v2/subscribers"
-    );
+    const handleSubmit = async (event) => {
+        event.preventDefault()
 
-    let headers = {
-        "Authorization": "Bearer [your-token]",
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    };
+        const url = new URL(
+            "https://api.sender.net/v2/subscribers"
+        );
 
-    let data = {
-        "email": "support@sender.net",
-        "firstname": "Sender",
-        "lastname": "Support",
-        "groups": ["eZVD4w", "b2vAR1"],
-        "fields": {"{$test_text}":"Documentation example","{$test_num}": 8}
-    };
+        let headers = {
+            "Authorization": `Bearer ${process.env.GATSBY_SENDER_TOKEN}`,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        };
 
-    fetch(url, {
-        method: "POST",
-        headers,body: JSON.stringify(data)
-    }).then(response => response.json());
+        let data = {
+            "email": email,
+            "firstname": fName,
+            "lastname": lName,
+            "groups": ["boQ4Ej"]
+        };
+        try {
+            fetch(url, {
+            method: "POST",
+            headers,body: JSON.stringify(data)
+        }).then(response => response.json());
+        } catch (e) {
+        
+        console.log('Error occurred during authentication');
+        }
+    }
 
 
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
           <p>Get Our Monthly Newsletter</p>
           <div className='name'>
                 <label>First Name
-                    <input type='text' name='fName'></input>
+                    <input 
+                        type='text'
+                        name='fName'
+                        onChange={(e) => {
+                            setFName(e.target.value);
+                        }}
+                        value={fName}
+                    ></input>
                 </label>
-            <label>Last Name
-          <input type='text' name='lName'></input>
+                <label>Last Name
+                    <input
+                        type='text'
+                        name='lName'
+                        onChange={(e) => {
+                            setLName(e.target.value);
+                        }}
+                        value={lName}
+                    ></input>
           </label>
           </div>
-          <label for='email'>Email Address</label>
-          <input type='email' id='email'></input>
-          <input type="submit" value="Subscribe"/>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            id='email'
+            onChange={(e) => {
+                setEmail(e.target.value);
+            }}
+            value={email}
+        ></input>
+          <button className='lessButton' type='submit'>Subscribe</button>
       </form>
   )
 }
 
-export default SenderEmail
+export default SenderEmail;
