b5ef8ba38a633a2cf9c1f7cb44dad7375c80eb05
Shelley Bassett <m.bassett@y7mail.com>
2022-03-01 17:14:12 +1100

    Added download pages

---
+++ src/templates/downloadTemplate.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { graphql } from "gatsby";
+// import SeoWithQuery from '../components/seo/seo'
+import { Helmet } from "react-helmet";
+import Headers from "../components/Headers";
+
+const Template = ({ data }) => {
+    const post = data.markdownRemark;
+    const page = data.site.siteMetadata;
+
+    const pageTitle = post.frontmatter.title + " || " + page.title;
+
+    return (
+        <>
+            <Helmet
+                title={pageTitle}
+                isBlogPost={true}
+                imageMeta={
+                    post.frontmatter.thumbnail.childImageSharp.original.src
+                }
+                meta={[
+                    {
+                        name: "description",
+                        content: post.frontmatter.description || post.excerpt,
+                    },
+                    {
+                        name: "image",
+                        content:
+                            post.frontmatter.thumbnail.childImageSharp.original
+                                .src,
+                    },
+                    { name: "datePublished", content: post.frontmatter.date },
+                ]}
+            ></Helmet>
+
+            <div className="blog-post-container">
+                <div className="blog-post">
+                    <Headers
+                        subtitle={post.frontmatter.date}
+                        title={post.frontmatter.title}
+                    />
+                    <div
+                        className="blog-post-content"
+                        dangerouslySetInnerHTML={{ __html: post.html }}
+                    ></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Template;
+
+export const pageQuery = graphql`
+    query DownloadByPath($pagePath: String) {
+        markdownRemark(frontmatter: { path: { eq: $pagePath } }) {
+            html
+            excerpt
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                path
+                title
+                description
+                download {
+                    absolutePath
+                    size
+                }
+            }
+        }
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`;
