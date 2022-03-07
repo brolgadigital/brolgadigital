f4f3e33708f3182a1237e9bf7ce14605ab4b2ec4
Shelley Bassett <m.bassett@y7mail.com>
2022-03-08 07:53:05 +1100

    Changed Cards to UiKit with Component

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "react-slideshow-image/dist/styles.css";
 import Headers from "../components/Headers";
-import Cards from "../components/Cards";
+import { InfoCard, CardWrapper } from "../components/Cards";
 
 export default function main() {
     return (
@@ -14,68 +14,68 @@ export default function main() {
                 </p>
 
                 <h2>Established Businesses Looking to Grow</h2>
-                <div className="cardwrapper">
-                    <Cards
+                <CardWrapper>
+                    <InfoCard
                         title="Ecommerce and Online Shopping"
                         icon="shopping-bag"
                         desc="Need an online shop or booking system? We can help build one tailored to your business needs."
                         info="/services/ecommerce"
                     />
-                    <Cards
+                    <InfoCard
                         title="Social Media and Marketing Strategies"
                         icon="users"
                         desc="Get help designing marketing plans and campaigns that will boost your business and generate valuable leads."
                         info="/services/strategy"
                     />
-                    <Cards
+                    <InfoCard
                         title="Usability, Speed and Search Engine Optimisations"
                         icon="activity"
                         desc="Organic web traffic is still best lead generator. We can enhance how your website appears in search results."
                         info="/services/seo"
                     />
-                </div>
+                </CardWrapper>
 
                 <h2>Artists and Creatives</h2>
-                <div className="cardwrapper">
-                    <Cards
+                <CardWrapper>
+                    <InfoCard
                         title="Micro and Static Sites"
                         icon="layout"
                         desc="Our websites are built to fit your business. Big our small, we can make something perfectly tailored to you."
                         info="/services/smallsite"
                     />
-                    <Cards
+                    <InfoCard
                         title="Ecommerce and Online Shopping"
                         icon="shopping-bag"
                         desc="Need an online shop or booking system? We can help build one tailored to your business needs."
                         info="/services/ecommerce"
                     />
-                </div>
+                </CardWrapper>
 
                 <h2>New Businesses and Startups</h2>
-                <div className="cardwrapper">
-                    <Cards
+                <CardWrapper>
+                    <InfoCard
                         title="Branding"
                         icon="gift"
                         desc="From a brand guide to business cards, social icons to marketing collateral, we'll present your story cohesively wherever your customers and clients are."
                         info="/services/branding"
                     />
-                    <Cards
+                    <InfoCard
                         title="Micro and Static Sites"
                         icon="layout"
                         desc="Our websites are built to fit your business. Big our small, we can make something perfectly tailored to you."
                         info="/services/smallsite"
                     />
-                </div>
+                </CardWrapper>
 
                 <h2>The Technologically Challenged</h2>
-                <div className="cardwrapper">
-                    <Cards
+                <CardWrapper>
+                    <InfoCard
                         title="Support and Maintenance"
                         icon="life-buoy"
                         desc="Not technically minded? Let us manage the hard parts, and explain things in easy-to-understand language."
                         info="/services/support"
                     />
-                </div>
+                </CardWrapper>
             </div>
         </>
     );
